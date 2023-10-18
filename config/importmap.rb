fb8a2c858537850efb365f2052601f2f5804dfc0
ines lucas <lucasinesmaria@gmail.com>
2023-10-18 14:53:33 +0100

    photo model created

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "three", to: "https://ga.jspm.io/npm:three@0.157.0/build/three.module.js"
