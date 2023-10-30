a8abe761e17b1fbee34eea79151137045ec909fe
ines lucas <lucasinesmaria@gmail.com>
2023-10-30 22:59:51 +0000

    testing image texture, undefined error

--- app/javascript/controllers/threejs_controller.js
+++ app/javascript/controllers/threejs_controller.js
@@ -1,9 +1,8 @@
-// Cube is commented but not deleted to show how to create a three.js cube in Rails
-
 import { Controller } from "@hotwired/stimulus";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples"; // as added permanently to importmaps - original: import { OrbitControls } from "https://ga.jspm.io/npm:three@0.157.0/examples/jsm/controls/OrbitControls.js";
 import GUI from 'lil-gui';
+// import imageFilename from 'assets/images/korean_couple.jpg';
 
 // Connects to data-controller="threejs"
 export default class extends Controller {
@@ -13,6 +12,11 @@ export default class extends Controller {
     // console.log(GUI);
 
     this.initThreeJS();
+
+    // ⬇️
+    // const images = this.data.get("images"); // .split(",") // to split the string into an array of strings/urls - REMOVE!
+    // const imageFilename = 'korean_couple.jpg';
+    // const imagePath = require(`assets/images/${imageFilename}`).default;
 
     window.addEventListener('resize', () => {
       console.log('window has been resized');
@@ -42,6 +46,10 @@ export default class extends Controller {
   }
 
   initThreeJS() {
+    /** Images testing ⬇️ */
+    // this.images = ['http://localhost:3000/rails/active_storage/disk/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDVG9JYTJWNVNTSWhaSEIyT0c5ek5YWTNPVEZ6Y21WNE5tRnVhR3N3YURocmJYcDZkd1k2QmtWVU9oQmthWE53YjNOcGRHbHZia2tpUDJsdWJHbHVaVHNnWm1sc1pXNWhiV1U5SWpBd01EQXpNQzVLVUVjaU95Qm1hV3hsYm1GdFpTbzlWVlJHTFRnbkp6QXdNREF6TUM1S1VFY0dPd1pVT2hGamIyNTBaVzUwWDNSNWNHVkpJZzlwYldGblpTOXFjR1ZuQmpzR1ZEb1JjMlZ5ZG1salpWOXVZVzFsT2dwc2IyTmhiQT09IiwiZXhwIjoiMjAyMy0xMC0yOVQxMTowMzowNy4zNTlaIiwicHVyIjoiYmxvYl9rZXkifX0=--88ac1a016b28f5c6e375e338cce404f749f20e1d/000030.JPG'];
+    // this.images = ['http://localhost:3000/images/korean_couple.jpg'];
+
     /** Debug */
     this.gui = new GUI();
 
@@ -59,15 +67,6 @@ export default class extends Controller {
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); // retina display - optimizing for performance, by creating a pixel ratio between own screens' and a maximum of 2
     document.body.appendChild(this.renderer.domElement);
 
-    /** Cube */
-    // this.geometry = new THREE.BoxGeometry();
-    // this.material = new THREE.MeshBasicMaterial( {
-    //   color: 0x00ff00,
-    //   wireframe: false
-    // } );
-
-    // this.originCube = this.createCube(0,0,0);
-
     /** Circle */
     const circleGeometry = new THREE.CircleGeometry( 2, 32 );
     this.wireframeMaterial = new THREE.MeshBasicMaterial( {
@@ -79,8 +78,13 @@ export default class extends Controller {
     // Rotate the circle around the X-axis by ~30 degrees
     this.circle.rotation.x = Math.PI / - 3;
 
-    // To rotate around the Y-axis, uncomment below:
+    // To rotate around the Y-axis, uncomment:
     // this.circle.rotation.y = Math.PI / 4;
+
+    // ⬇️
+    // this.images = JSON.parse(this.data.get("images"));
+    // console.log(this.images); // returns null
+    // console.log(this.images[0]); // returns an error
 
     /** Add Rectangles around the Circle */
     this.rectangles = this.addRectanglesToCircle(30, 2); // 8 rectangles, 2 of radius. Storing in a variable so we can access them outside the scope of this function (eg. for UI)
@@ -95,7 +99,6 @@ export default class extends Controller {
     this.axesHelper = new THREE.AxesHelper(5); // 5 = represents the size (length) of the axes
 
     this.scene.add(
-      // this.originCube,
       this.gridHelper,
       this.circle,
       this.axesHelper);
@@ -113,9 +116,16 @@ export default class extends Controller {
   addRectanglesToCircle(numberOfRectangles, circleRadius) {
     const rectangleHeight = 0.5; // modifying the height of the rectangles
     const rectangleWidth = 1
+
+    /** loading image as a texture */
+    const texture = new THREE.TextureLoader().load('https://play-lh.googleusercontent.com/1-hPxafOxdYpYZEOKzNIkSP43HXCNftVJVttoo4ucl7rsMASXW3Xr6GlXURCubE1tA=w3840-h2160-rw'); // this.images[0] - I removed the index 0 and it got better
+    // console.log(this.images[0]);
+    // console.log(this.images); // returns what seems like an actual object
+
+    const rectangleMaterial = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide }); // ensures that the material renders on both sides of our 2D plane
     const rectangleGeometry = new THREE.PlaneGeometry(rectangleWidth, rectangleHeight);
-    const rectangleMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide }); // ensuring that the material renders on both sides of our 2D plane
-    const rectangles = []; // to store rectangles newly created
+    // const rectangle = new THREE.Mesh(rectangleGeometry, rectangleMaterial);
+    const rectangles = []; // to store rectangles newly created, this is what gets called in the initThreeJS() function
 
     for (let i = 0; i < numberOfRectangles; i++) { // = we loop as many times as the number of rectangles we want to create
       const theta = (i / numberOfRectangles) * 2 * Math.PI; // angle between each rectangle
@@ -136,13 +146,7 @@ export default class extends Controller {
     return rectangles;
   }
 
-  // createCube(x,y,z) {
-  //   const cube = new THREE.Mesh( this.geometry, this.material );
-  //   cube.position.set(x,y,z);
-  //   return cube;
-  // }
-
-  animate() { // to fix distortion of cube object
+  animate() {
     // This creates a loop that causes the renderer to draw the scene every time the screen is refreshed (typically 60 times per second)
     requestAnimationFrame(this.animate.bind(this));
 
@@ -150,4 +154,3 @@ export default class extends Controller {
     this.renderer.render(this.scene, this.camera);
   }
 }
-
