137122e5374e86346213d8c8ce4eea85045f992c
ines lucas <lucasinesmaria@gmail.com>
2023-10-30 23:44:15 +0000

    first commit after trial with pre compilation of assets and a string of errors, back to 0

--- app/javascript/controllers/threejs_controller.js
+++ app/javascript/controllers/threejs_controller.js
@@ -2,21 +2,12 @@ import { Controller } from "@hotwired/stimulus";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples"; // as added permanently to importmaps - original: import { OrbitControls } from "https://ga.jspm.io/npm:three@0.157.0/examples/jsm/controls/OrbitControls.js";
 import GUI from 'lil-gui';
-// import imageFilename from 'assets/images/korean_couple.jpg';
 
 // Connects to data-controller="threejs"
 export default class extends Controller {
   connect() {
-    console.log("Hello, Stimulus!", this.element);
-    // console.log(THREE.OrbitControls);
-    // console.log(GUI);
-
+    console.log("Hello, Stimulus!", this.element); // console.log(THREE.OrbitControls); // console.log(GUI);
     this.initThreeJS();
-
-    // ⬇️
-    // const images = this.data.get("images"); // .split(",") // to split the string into an array of strings/urls - REMOVE!
-    // const imageFilename = 'korean_couple.jpg';
-    // const imagePath = require(`assets/images/${imageFilename}`).default;
 
     window.addEventListener('resize', () => {
       console.log('window has been resized');
@@ -46,17 +37,12 @@ export default class extends Controller {
   }
 
   initThreeJS() {
-    /** Images testing ⬇️ */
-    // this.images = ['http://localhost:3000/rails/active_storage/disk/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDVG9JYTJWNVNTSWhaSEIyT0c5ek5YWTNPVEZ6Y21WNE5tRnVhR3N3YURocmJYcDZkd1k2QmtWVU9oQmthWE53YjNOcGRHbHZia2tpUDJsdWJHbHVaVHNnWm1sc1pXNWhiV1U5SWpBd01EQXpNQzVLVUVjaU95Qm1hV3hsYm1GdFpTbzlWVlJHTFRnbkp6QXdNREF6TUM1S1VFY0dPd1pVT2hGamIyNTBaVzUwWDNSNWNHVkpJZzlwYldGblpTOXFjR1ZuQmpzR1ZEb1JjMlZ5ZG1salpWOXVZVzFsT2dwc2IyTmhiQT09IiwiZXhwIjoiMjAyMy0xMC0yOVQxMTowMzowNy4zNTlaIiwicHVyIjoiYmxvYl9rZXkifX0=--88ac1a016b28f5c6e375e338cce404f749f20e1d/000030.JPG'];
-    // this.images = ['http://localhost:3000/images/korean_couple.jpg'];
-
     /** Debug */
     this.gui = new GUI();
 
-    // code to initialize ThreeJS
+    /** Scene, Camera, Renderer */
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setClearColor(0xFFFFFF); // 0xFFFFFF = white
     this.sizes = {
@@ -80,11 +66,6 @@ export default class extends Controller {
 
     // To rotate around the Y-axis, uncomment:
     // this.circle.rotation.y = Math.PI / 4;
-
-    // ⬇️
-    // this.images = JSON.parse(this.data.get("images"));
-    // console.log(this.images); // returns null
-    // console.log(this.images[0]); // returns an error
 
     /** Add Rectangles around the Circle */
     this.rectangles = this.addRectanglesToCircle(30, 2); // 8 rectangles, 2 of radius. Storing in a variable so we can access them outside the scope of this function (eg. for UI)
@@ -116,15 +97,8 @@ export default class extends Controller {
   addRectanglesToCircle(numberOfRectangles, circleRadius) {
     const rectangleHeight = 0.5; // modifying the height of the rectangles
     const rectangleWidth = 1
-
-    /** loading image as a texture */
-    const texture = new THREE.TextureLoader().load('https://play-lh.googleusercontent.com/1-hPxafOxdYpYZEOKzNIkSP43HXCNftVJVttoo4ucl7rsMASXW3Xr6GlXURCubE1tA=w3840-h2160-rw'); // this.images[0] - I removed the index 0 and it got better
-    // console.log(this.images[0]);
-    // console.log(this.images); // returns what seems like an actual object
-
-    const rectangleMaterial = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide }); // ensures that the material renders on both sides of our 2D plane
     const rectangleGeometry = new THREE.PlaneGeometry(rectangleWidth, rectangleHeight);
-    // const rectangle = new THREE.Mesh(rectangleGeometry, rectangleMaterial);
+    const rectangleMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide }); // ensures that the material renders on both sides of our 2D plane // where texture will eventually be plugged
     const rectangles = []; // to store rectangles newly created, this is what gets called in the initThreeJS() function
 
     for (let i = 0; i < numberOfRectangles; i++) { // = we loop as many times as the number of rectangles we want to create
