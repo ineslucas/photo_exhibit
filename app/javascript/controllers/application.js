6bffcb82b96a5a91cae15ee96ea7228240271b71
ines lucas <lucasinesmaria@gmail.com>
2023-11-06 17:36:37 +0000

    rebundling without the foreman gem

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,4 +1,5 @@
 import { Application } from "@hotwired/stimulus"
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
 const application = Application.start()
 
--- config/importmap.rb
+++ config/importmap.rb
@@ -3,10 +3,10 @@
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+# pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "three", to: "https://ga.jspm.io/npm:three@0.157.0/build/three.module.js"
-pin "three/examples", to: "https://ga.jspm.io/npm:three@0.157.0/examples/jsm/controls/OrbitControls.js";
+# pin "three/examples", to: "https://ga.jspm.io/npm:three@0.157.0/examples/jsm/controls/OrbitControls.js";
 pin "lil-gui", to: "https://cdn.jsdelivr.net/npm/lil-gui@0.18/+esm";
